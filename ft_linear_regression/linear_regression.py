ee6f33a61a88e865c35f9b6a0f99446248dfb9f6
Darren WAN <dwan@e2r8p20.42.fr>
2018-10-03 16:30:56 +0200

    viz

--- ft_linear_regression/linear_regression.py
+++ ft_linear_regression/linear_regression.py
@@ -3,6 +3,10 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
 class LinearRegression:
     max_epochs = 10000
     precision= 0.00001
@@ -10,36 +14,31 @@ class LinearRegression:
     def __init__(self):
         self.theta0 = 0
         self.theta1 = 0
+        self.theta0_norm = 0
+        self.theta1_norm = 0
+        self.log = []
         self.data = {}
 
     def read_csv(self, path):
-        #km=[]
-        #price=[]
-        #with open(path, 'r') as f:
-        #    for idx, line in enumerate(f):
-        #        if idx > 0:
-        #            vals=line.rstrip('\n').split(',')
-        #            km.append(int(vals[0]))
-        #            price.append(int(vals[1]))
         return np.genfromtxt(path, delimiter=',', skip_header=1)
-        #return {"km": km, "price": price, "km_norm": [(i-np.mean(km))/np.std(km) for i in km], "price_norm": [(i-np.mean(price))/np.std(price) for i in price]}
 
-    def estimate_price(self, mileage):
-        return self.theta0 + self.theta1 * mileage
+    def estimate_price(self, theta0, theta1, mileage):
+        return theta0 + theta1 * mileage
 
     def train_model(self, path, learningRate, reset_theta=True, plot=False):
         def reconstruct_regressor():
-            return ((self.theta0 - self.theta1 * np.mean(self.data[:,0]) / np.std(self.data[:,0])) * np.std(self.data[:,1]) + np.mean(self.data[:,1]),
-                    self.theta1 / np.std(self.data[:,0]) * np.std(self.data[:,1]))
+            return ((self.theta0_norm - self.theta1_norm * np.mean(self.data[:,0]) / np.std(self.data[:,0])) * np.std(self.data[:,1]) + np.mean(self.data[:,1]),
+                    self.theta1_norm / np.std(self.data[:,0]) * np.std(self.data[:,1]))
 
         if reset_theta:
-            self.theta0 = 0
-            self.theta1 = 0
+            self.log = []
+            self.theta0_norm = 0
+            self.theta1_norm = 0
 
         self.data = self.read_csv(path)
         m = len(self.data)
 
-        print(self.data)
+        #print(self.data)
 
         if plot:
             plt.plot(self.data[:,0], self.data[:,1], 'ro')
@@ -50,10 +49,12 @@ class LinearRegression:
         price_norm = (self.data[:,1]-np.mean(self.data[:,1])) / np.std(self.data[:,1])
 
         for i in range(self.max_epochs):
-            tmp_theta0 = learningRate * sum(self.estimate_price(km_norm) - price_norm) / m
-            tmp_theta1 = learningRate * sum((self.estimate_price(km_norm) - price_norm) * km_norm) / m
-            self.theta0 -= tmp_theta0
-            self.theta1 -= tmp_theta1
+            tmp_theta0 = learningRate * sum(self.estimate_price(self.theta0_norm,self.theta1_norm,km_norm) - price_norm) / m
+            tmp_theta1 = learningRate * sum((self.estimate_price(self.theta0_norm,self.theta1_norm,km_norm) - price_norm) * km_norm) / m
+            self.theta0_norm -= tmp_theta0
+            self.theta1_norm -= tmp_theta1
+
+            self.log.append((self.theta0_norm,self.theta1_norm))
 
             if plot and (i%10 == 0):
                 y_vals = reconstruct_regressor()[0] + reconstruct_regressor()[1] * x_vals
@@ -67,6 +68,13 @@ class LinearRegression:
         self.theta1 = reconstruct_regressor()[1]
 
     def visualize(self):
+        def er_flat(theta0_tmp, theta1_tmp,x):
+            m=len(self.data)
+            km_norm = (x[:, 0] - np.mean(x[:, 0])) / np.std(x[:, 0])
+            price_norm = (x[:, 1] - np.mean(x[:, 1])) / np.std(x[:, 1])
+
+            return 1 / (2 * m) * sum((self.estimate_price(theta0_tmp, theta1_tmp, km_norm) - price_norm) ** 2)
+
         plt.plot(self.data[:,0], self.data[:,1], 'ro')
         axes = plt.gca()
         x_vals = np.array(axes.get_xlim())
@@ -74,4 +82,69 @@ class LinearRegression:
         plt.plot(x_vals, y_vals, '--')
         plt.show()
 
+        theta0_tmp = np.arange(-1, 1, 0.01)
+        theta1_tmp = np.arange(-1, 1, 0.01)
+        x, y = np.meshgrid(theta0_tmp, theta1_tmp)
 
+        z = np.zeros(shape=x.shape)
+        for i in range(z.shape[0]):
+            for j in range(z.shape[1]):
+                z[i, j] = er_flat(x[i, j], y[i, j],self.data)
+        print(z)
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        surf = ax.plot_surface(x, y, z, cmap=cm.inferno,
+                               linewidth=0, antialiased=False)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+        fig.colorbar(surf, shrink=0.8)
+
+        #log0 = [i[0] for i in self.log]
+        #log1 = [i[1] for i in self.log]
+        #z2 = np.zeros(len(self.log))
+        #for i in range(len(z2)):
+        #    z2[i] = er_flat(log0[i], log1[i])
+        #ax.plot(log0, log1, z2, linestyle='--', marker='3', color='c', markersize=0.1)
+
+        plt.show()
+
+
+
+
+
+
+#tmp=LinearRegression()
+#tmp.train_model('data/data.csv',0.01)
+#tmp.visualize()
+#
+#x=tmp.data
+#def er_flat(theta0_tmp, theta1_tmp):
+#    m = len(tmp.data)
+#    km_norm = (tmp.data[:, 0] - np.mean(tmp.data[:, 0])) / np.std(tmp.data[:, 0])
+#    price_norm = (tmp.data[:, 1] - np.mean(tmp.data[:, 1])) / np.std(tmp.data[:, 1])
+#    return 1/(2*m) * sum((estimate_price(km_norm,theta0_tmp, theta1_tmp) - price_norm)**2)
+#
+#theta0_tmp = np.arange(-1,1,0.01)
+#theta1_tmp = np.arange(-1,1,0.01)
+#x,y=np.meshgrid(theta0_tmp,theta1_tmp)
+#
+#z=np.zeros(shape=x.shape)
+#for i in range(z.shape[0]):
+#    for j in range(z.shape[1]):[i,j] = er_flat(x[i,j],y[i,j])
+#
+#
+#fig = plt.figure()
+#ax = fig.gca(projection='3d')
+#surf = ax.plot_surface(x, y, z, cmap=cm.inferno,
+#                       linewidth=0, antialiased=False)
+#
+##ax.set_zlim(0, 3.01)
+#ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+#
+## Add a color bar which maps values to colors.
+#fig.colorbar(surf, aspect=5)
+#
+#plt.show()
+#
--- working.py
+++ working.py
@@ -1,8 +1,11 @@
+# import imp
+# imp.reload(linear_regression)
 from ft_linear_regression import linear_regression
 
 tmp = linear_regression.LinearRegression()
 
 tmp.train_model('data/data.csv',0.01,plot=True)
 print('{}, {}'.format(tmp.theta0, tmp.theta1))
+print('{}, {}'.format(tmp.theta0_norm, tmp.theta1_norm))
 
 tmp.visualize()
