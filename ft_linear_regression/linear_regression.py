18f82f4f41366a6bf83897ff11cf82464b975ea2
Viktor MALESEVIC <vmalesev@e2r9p18.42.fr>
2018-10-03 14:21:37 +0200

    description for read_csv and train_model

--- ft_linear_regression/linear_regression.py
+++ ft_linear_regression/linear_regression.py
@@ -6,8 +6,11 @@ from matplotlib import pyplot as plt
 class LinearRegression:
 
     """
-    LinearRegression is defined as class with severl properties useful for data reading & scaling,
-    fitting the parameters
+    LinearRegression is defined as class with several properties useful for data reading & scaling,
+    fitting the parameters, and finally plot the results of the linear regression versus the training data.
+    
+    NOTA: Although an analytical solution exists for linear regressions, the exercise imposes to use
+    gradient descent.     
     """
 
     def __init__(self):
@@ -16,24 +19,36 @@ class LinearRegression:
         self.data = {}
 
     def read_csv(self, path):
-        #km=[]
-        #price=[]
-        #with open(path, 'r') as f:
-        #    for idx, line in enumerate(f):
-        #        if idx > 0:
-        #            vals=line.rstrip('\n').split(',')
-        #            km.append(int(vals[0]))
-        #            price.append(int(vals[1]))
+        """
+        This functions uses numpy's genfromtxt function in order to read the data
+        :param path: an OS path representing the name of the file
+        :return: an array in the dimensions of the csv file
+        """
         return np.genfromtxt(path, delimiter=',', skip_header=1)
-        #return {"km": km, "price": price, "km_norm": [(i-np.mean(km))/np.std(km) for i in km], "price_norm": [(i-np.mean(price))/np.std(price) for i in price]}
 
     def estimate_price(self, mileage):
         return self.theta0 + self.theta1 * mileage
 
-    def train_model(self, path, learningRate, max_epochs=10000, precision=0.00001, reset_theta=True, plot=False):
+    def train_model(self, path, learning_rate=0.001, max_epochs=10000, precision=0.00001, reset_theta=True, plot=False):
+        """
+        This function does the gradient descent to find the right parameters theta0 and theta1
+        It first scales the data. Then applies a gradient descent with a number of steps limited by max_epochs.
+        The algorithm stops it's search when the difference between to steps for theta0 and theta1 gets
+        below 'precision'.
+
+        :param path: an OS path representing the name of the file
+        :param learning_rate: the size of the steps to take at each epoch. (float) A large step makes the convergence
+        potentially faster but might also not reach convergence at all.
+        :param max_epochs: the maximum number of iterations. (int)
+        :param precision: the desired precision. (float)
+        :param reset_theta: True or False. If True, the previous values calculated for theta0 and theta1 will be erased
+        :param plot: True or False. If True, will plot the results.
+        :return: theta0 & theta1
+        """
         def reconstruct_regressor():
-            return ((self.theta0 - self.theta1 * np.mean(self.data[:,0]) / np.std(self.data[:,0])) * np.std(self.data[:,1]) + np.mean(self.data[:,1]),
-                    self.theta1 / np.std(self.data[:,0]) * np.std(self.data[:,1]))
+            return ((self.theta0 - self.theta1 * np.mean(self.data[:, 0]) / np.std(self.data[:, 0])) *
+                    np.std(self.data[:, 1]) + np.mean(self.data[:, 1]),
+                    self.theta1 / np.std(self.data[:, 0]) * np.std(self.data[:, 1]))
 
         if reset_theta:
             self.theta0 = 0
@@ -45,40 +60,33 @@ class LinearRegression:
         print(self.data)
 
         if plot:
-            plt.plot(self.data[:,0], self.data[:,1], 'ro')
+            plt.plot(self.data[:, 0], self.data[:, 1], 'ro')
             axes = plt.gca()
             x_vals = np.array(axes.get_xlim())
 
-<<<<<<< HEAD
-        for i in range(max_epochs):
-            tmp_theta0 = learningRate * sum(
-                [self.estimate_price(self.data['km_norm'][i]) - self.data['price_norm'][i] for i in range(m)]) / m
-            tmp_theta1 = learningRate * sum(
-                [(self.estimate_price(self.data['km_norm'][i]) - self.data['price_norm'][i]) * self.data['km_norm'][i] for i in range(m)]) / m
-=======
-        km_norm = (self.data[:,0]-np.mean(self.data[:,0])) / np.std(self.data[:,0])
-        price_norm = (self.data[:,1]-np.mean(self.data[:,1])) / np.std(self.data[:,1])
->>>>>>> 3a8538a6f625005aabffbb005f1beb0a4a0b4222
+        km_norm = (self.data[:, 0]-np.mean(self.data[:, 0])) / np.std(self.data[:, 0])
+        price_norm = (self.data[:, 1]-np.mean(self.data[:, 1])) / np.std(self.data[:, 1])
 
-        for i in range(self.max_epochs):
-            tmp_theta0 = learningRate * sum(self.estimate_price(km_norm) - price_norm) / m
-            tmp_theta1 = learningRate * sum((self.estimate_price(km_norm) - price_norm) * km_norm) / m
+        for i in range(max_epochs):
+            tmp_theta0 = learning_rate * sum(self.estimate_price(km_norm) - price_norm) / m
+            tmp_theta1 = learning_rate * sum((self.estimate_price(km_norm) - price_norm) * km_norm) / m
             self.theta0 -= tmp_theta0
             self.theta1 -= tmp_theta1
 
-            if plot and (i%10 == 0):
+            if plot and (i % 10 == 0):
                 y_vals = reconstruct_regressor()[0] + reconstruct_regressor()[1] * x_vals
                 plt.plot(x_vals, y_vals, '--')
 
             if (abs(tmp_theta0) < precision) and (abs(tmp_theta1) < precision):
                 print('epochs:' + str(i))
                 break
-        if plot: plt.show()
+        if plot: 
+            plt.show()
         self.theta0 = reconstruct_regressor()[0]
         self.theta1 = reconstruct_regressor()[1]
 
     def visualize(self):
-        plt.plot(self.data[:,0], self.data[:,1], 'ro')
+        plt.plot(self.data[:, 0], self.data[:, 1], 'ro')
         axes = plt.gca()
         x_vals = np.array(axes.get_xlim())
         y_vals = self.theta0 + self.theta1 * x_vals
